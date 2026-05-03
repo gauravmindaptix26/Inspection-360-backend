24adbe9890cd79d0019db5b2f7d5e1a3b4d678c3
gauravchauhan26 <gauravsingh42022@gmail.com>
2026-05-03 18:20:58 +0530

    hlo

--- inspection_project/settings.py
+++ inspection_project/settings.py
@@ -262,3 +262,6 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
+
+STATIC_URL = 'static/' # This is probably already here
+STATIC_ROOT = BASE_DIR / 'static' # Add this line!
